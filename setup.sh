dc70de32a95518317c1fc1375cfcf4707f586bc7
spectatora <spectatora_91@abv.bg>
2024-09-09 22:00:15 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 npm install wrangler --save-dev
-composer create-project drupal/recommended-project drupal_site
+composer create-project drupal/recommended-project:10.3.3  drupal_site
 cd drupal_site
 composer require drush/drush
+composer require drupal/tome
 vendor/bin/drush si --db-url=sqlite://drupal:drupal@127.0.0.1:3306/drupal
 vendor/bin/drush upwd admin 123
-vendor/bin/drush generate phpstorm-metadata
+vendor/bin/drush cr
 
 echo 'export PATH="./vendor/bin:$PATH"' >> ~/.bashrc
 export PATH="./vendor/bin:$PATH
